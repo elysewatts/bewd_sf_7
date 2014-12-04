aee57ca7a178043fbeef692c51d07e67f067b953
Elyse Watts <wattsupelyse@gmail.com>
2014-12-03 18:55:58 -0800

    first commit

---
+++ 02_Variables_Conditionals/starter_code/HW.rb
@@ -0,0 +1,31 @@
+def multiply(num1, num2)
+	num1 * num2
+end
+
+def divide(num1, num2)
+    num1/num2
+
+end
+
+def remainder(num1, num2)
+	#modulo
+end
+
+def add(num1, num2)
+
+end
+
+#conditional that checks for type integer
+#prompt
+
+result = multiply(1, 4)
+puts "#{num1} * #{num2} is #{result}"
+
+result = divide(1, 4)
+puts "#{num1} divided #{num2}is #{result}"
+
+result = remainder(1, 4)
+puts "the result of #{num1} divided #{num2}is #{result}"
+
+result = add(1, 4)
+puts "#{num1} plus #{num2}is #{result}"
---
+++ 02_Variables_Conditionals/starter_code/calc.rb
@@ -0,0 +1,6 @@
+def multiply(num1, num2)
+	num1 * num2
+end
+
+result = multiply(1, 4)
+puts "The result is #{result}"
